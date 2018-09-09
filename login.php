540655100873f276b3c4d8d2a4ec04dab5e1eb60
alextremo29 <alexnarfo5@gmail.com>
2018-09-09 13:03:46 -0500

    cambio login

--- login.php
+++ login.php
@@ -98,5 +98,10 @@
 		</div>
 	</div>
 </body>
+<footer class="py-3" style="bottom: 0px; background-color: #193338;">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; Bambusa.Digital 2018</p>
+  </div>
+</footer>
 <script src="js/funciones_usuario.js"></script>
 </html>
